6716581fbadbd76a7d9d9ffeeed8f2c9adf6fdd9
Ignatius Timothy Manullang <13517044@std.stei.itb.ac.id>
2020-10-23 01:47:41 +0700

    fixed counter mode

--- feistelCipher.py
+++ feistelCipher.py
@@ -12,9 +12,14 @@ def encryptMessage(key, message, mode):
     if ( lengthOfLastBlock < BLOCKSIZE):
         for i in range(lengthOfLastBlock, BLOCKSIZE):
             message[len(message)-1] += " "
-    messagetemp = ""
+    # generate a 256 bit key based of user inputted key
+    key = key_256(key)
+    key_initial = key
+    k = 0
+    messagetemp = []
     if mode == "counter":
-        messagetemp = message
+        for block in message:
+            messagetemp.append(block)
         for i in range(len(message)):
             message[i] = bin(i)[2:]
             print(message[i])
@@ -22,10 +27,6 @@ def encryptMessage(key, message, mode):
             if ( len(message[i]) < BLOCKSIZE):
                 for j in range(len(message[i]), BLOCKSIZE):
                     message[i] = "0" + message[i]
-        print("message encrypt = " , message)
-    # generate a 256 bit key based of user inputted key
-    #key = key_256(key)
-    key_initial = key
     for block in message:
         L = [""] * (ROUNDS + 1)
         R = [""] * (ROUNDS + 1)
@@ -39,12 +40,11 @@ def encryptMessage(key, message, mode):
                 else:
                     key = subkeygen(L[i], key_initial, i) 
             R[i] = xor(L[i - 1], scramble(R[i - 1], i, key))
-        ciphertext += (L[ROUNDS] + R[ROUNDS])
-    if mode == "counter":
-        messagetemp = "".join(messagetemp)
-        print(messagetemp)
-        print(len(messagetemp))
-        ciphertext = xor(messagetemp, ciphertext)
+        block_ciphertext = (L[ROUNDS] + R[ROUNDS])
+        if mode == "counter":
+            block_ciphertext = xor(messagetemp[k], block_ciphertext)
+            k += 1
+        ciphertext += block_ciphertext
     return ciphertext
 
 def decryptCipher(key, ciphertext, mode):
@@ -56,50 +56,54 @@ def decryptCipher(key, ciphertext, mode):
     if ( lengthOfLastBlock < BLOCKSIZE):
         for i in range(lengthOfLastBlock, BLOCKSIZE):
             ciphertext[len(ciphertext)-1] += " "
-    ciphertexttemp = ""
+    # generate a 256 bit key based off the user inputted key
+    key = key_256(key)
+    key_initial = key
+    k = 0
+    ciphertexttemp = []
     if mode == "counter":
-        ciphertexttemp = ciphertext
+        for block in ciphertext:
+            ciphertexttemp.append(block)
         for i in range(len(ciphertext)):
             ciphertext[i] = bin(i)[2:]
             if ( len(ciphertext[i]) < BLOCKSIZE):
                 print(ciphertext[i])
                 for j in range(len(ciphertext[i]), BLOCKSIZE):
                     ciphertext[i] = "0" + ciphertext[i]
-        print("ciphertext decrypt = " , ciphertext)
-    # generate a 256 bit key based off the user inputted key
-    #key = key_256(key)
-    key_initial = key
-    for block in ciphertext:
-        L = [""] * (ROUNDS + 1)
-        R = [""] * (ROUNDS + 1)
-        L[ROUNDS] = block[0:BLOCKSIZE//2]
-        R[ROUNDS] = block[BLOCKSIZE//2:BLOCKSIZE]
-        for i in range(8, 0, -1):
-            if (mode == "cbc"):
-                key = subkeygen(L[i], key_initial, i)
-                if (i == 1):
-                    key = key_initial
-            R[i-1] = L[i]
-            L[i-1] = xor(R[i], scramble(L[i], i, key))
-        message += (L[0] + R[0])
-    if mode == "counter":
-        ciphertexttemp = "".join(ciphertexttemp)
-        message = xor(ciphertexttemp,message)
+        for block in ciphertext:
+            L = [""] * (ROUNDS + 1)
+            R = [""] * (ROUNDS + 1)
+            L[0] = block[0:BLOCKSIZE//2]
+            R[0] = block[BLOCKSIZE//2:BLOCKSIZE]
+            for i in range(1, ROUNDS+1):
+                L[i] = R[i - 1]
+                R[i] = xor(L[i - 1], scramble(R[i - 1], i, key))
+            block_message = (L[ROUNDS] + R[ROUNDS])
+            block_message = xor(ciphertexttemp[k], block_message)
+            message += block_message
+            k += 1
+    else:
+        for block in ciphertext:
+            L = [""] * (ROUNDS + 1)
+            R = [""] * (ROUNDS + 1)
+            L[ROUNDS] = block[0:BLOCKSIZE//2]
+            R[ROUNDS] = block[BLOCKSIZE//2:BLOCKSIZE]
+            for i in range(8, 0, -1):
+                if (mode == "cbc"):
+                    key = subkeygen(L[i], key_initial, i)
+                    if (i == 1):
+                        key = key_initial
+                R[i-1] = L[i]
+                L[i-1] = xor(R[i], scramble(L[i], i, key))
+            block_message = (L[0] + R[0])
+            message += block_message
     return message
 
-
-# def key_256(key):
-#     bkey= key + SECRET
-#     return hashlib.sha256(bkey.encode()).hexdigest()
+def key_256(key):
+    return hashlib.sha256(key.encode()).hexdigest()
 
 def subkeygen(s1, s2, i):
-    #raise ValueError("CANNOT ACCESS")
-    #print ("GENERATING KEY #" + str(i))
-    #print ("S1: " + s1)
-    #print ("S2: " + s2)
-    result = hashlib.sha256(s1.encode('utf-8') + s2.encode('utf-8')).hexdigest()
-    #print ("RESULT: " + result)
-    return result
+    return hashlib.sha256(s1.encode() + s2.encode()).hexdigest()
 
 def scramble(block, NFeistelRound, key):
     key = stringToBinary(key)
@@ -151,7 +155,11 @@ def binaryToHex(binaryString):
     return hexadecimal
 
 if __name__ == "__main__":
-    ciphertext = encryptMessage("B", "AA", "counter")
+    #ciphertext = encryptMessage("B", "00000000", "counter")
+    ciphertext = encryptMessage("B", "HELLOWORLDTIMTAM", "counter")
+    #ciphertext = encryptMessage("B", "AAAAAAAA", "ebc")
     print("ciphertext = ", ciphertext)
     #print("AA = ", binaryToHex('10101010')[2:].upper())cl
-   # print("plaintext = ", decryptCipher("B", ciphertext, "counter"))
+    plaintext = decryptCipher("B", ciphertext, "counter")
+    print("plaintext = ", plaintext, " ", len(plaintext))
+    #print("plaintext = ", decryptCipher("B", ciphertext, "ebc"))
